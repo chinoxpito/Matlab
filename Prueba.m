6e02ea352a2c0cd498e6b66b550c998dea2cae7b
Chinaski <jonathan.acunah@utem.cl>
2017-09-12 02:06:34 -0300

    Captura y grabado

--- Prueba.m
+++ Prueba.m
@@ -1,14 +1,31 @@
 clc
 clear all
-Fs = 44100;
 
-recObj= audiorecorder;
-disp('Grabando...');
-recordblocking(recObj,3);
-disp('Fin de la Grabacion.');
+%Fs = 44100;
+%samples = [2*Fs];
+
+%recObj= audiorecorder;
+
+%disp('Grabando...');
+%recordblocking(recObj,3);
+%disp('Fin de la Grabacion.');
+fs=44100; %f. muestreo
+senal_salida=audiorecorder(fs,16,1);%Creacion del objeto de grabacion
+msgbox('Empezando Grabacion',' Grabadora '); %Mensaje de informacion
+recordblocking(senal_salida,5);%Grabacion del sonido
+msgbox('Terminando Grabacion',' Grabadora ');%Mensaje de informacion
+%Paso los valores del objeto a una seal
+senal_grabada=getaudiodata(senal_salida, 'single');
+%Grabamos y guardamos la seal
+y=senal_grabada
+filename = 'hola.wav';
+audiowrite(filename,y,fs);
+
+plot(y);
+sound(y,fs); 
 
 %play(recObj);
 
-myRecording = getaudiodata(recObj);
+%myRecording = getaudiodata(recObj);
 
-plot(myRecording);
+%plot(myRecording);
---
+++ main.m
@@ -0,0 +1,26 @@
+clear all
+clc
+
+disp('Iniciar...');
+
+Fs = 44100;
+%Fs = input('Ingrese Fs:');
+t= input('Ingrese Tiempo de muestreo:');
+recObj = audiorecorder(Fs, 16, 1);%Creacion del objeto de grabacion
+% Record your voice for 3 seconds.
+disp('Start speaking.');%Mensaje de informacion
+recordblocking(recObj, t);%Grabacion del sonido
+disp('End of Recording.');%Mensaje de informacion
+
+
+%Paso los valores del objeto a una seal
+y=getaudiodata(recObj, 'single');
+
+%Grabamos y guardamos la seal
+filename = 'Prueba.wav';
+audiowrite(filename,y,Fs);
+
+% Plot the waveform.
+plot(y);
+% Play back the recording.
+sound(y,Fs);
